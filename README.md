a151129fa12e2b0fe7886f0d99ee38503aa63614
Marc Wright <marc.wright@gmail.com>
2019-12-06 09:25:24 -0500

    Add axios and endpoints

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+
+[Louisville Open Data](https://data.louisvilleky.gov/search/type/dataset)
+
+
+1. [Services Legacy API Endpoint](https://data.louisvilleky.gov/dataset/louisville-metro-services-lookup/resource/ae18fe6a-2afd-4385-894c-2a7234a81d90)
+
+2. [Metro Online Services](https://data.louisvilleky.gov/dataset/louisville-metro-online-services/resource/7103297f-2fdf-4e17-aca3-2e50732ada79)
+
+3. [Dockless Vehicles](https://data.louisvilleky.gov/dataset/dockless-vehicles)
+
+4. [Air Quality API](https://data.louisvilleky.gov/dataset/local-air-quality-api/resource/1e482601-9848-4f86-92a4-203180ef74de)
+
+5. [Dockless Vehicle Trips Block Level](https://data.louisvilleky.gov/node/26501/api)
+[Trip Duration](https://data.louisvilleky.gov/api/action/datastore/search.json?resource_id=e36546f6-888b-4e66-8a87-9b68cab471e6&limit=10)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+const cors = 'https://cors-anywhere.herokuapp.com/'
+const servicesLegacyEndpoint = 'https://api.louisvilleky.gov/api/geo/FindQuickServiceLookup?address=121%20FREEMAN%20AVE&city=Louisville';
+const metroOnlineServicesEndpoint = 'https://louisvilleky.gov/services/toolbox_services.json?limit=0';
+const airQualityApiEndpoint = 'https://aaws.louisvilleky.gov/api/v1/Monitor/CityAQI';
+const birdApiEndpoint = 'https://mds.bird.co/gbfs/louisville/free_bikes';
+const limeApiEndpoint = 'https://data.lime.bike/api/partners/v1/gbfs/louisville/free_bike_status';
+const tripDuration = 'https://data.louisvilleky.gov/api/action/datastore/search.json?resource_id=e36546f6-888b-4e66-8a87-9b68cab471e6&limit=10';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData() {
+    console.log("line 13")
+    axios({
+      url: `${cors}${birdApiEndpoint}`,
+      method: 'get'
+    })
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <h1>Louisville Data Hackathon Starter</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
